948b3027d3af964f856115d96c51578cf7b9c91d
sam <samuel13241@gmail.com>
2023-01-26 15:21:38 -0800

    NavBar, Footer 80% done

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Project Title
 
-SunriseUMC webpage
+
 
 ## Description
 
---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("init");
+
+index = {
+    init: function(){
+        this.second_nav_bar_show();
+    },
+
+    second_nav_bar_show: function(){
+        $('.header__menus>ul>li').click(function(){
+            let target =$(this).children().eq(1);
+            if(!target.hasClass('active')){
+           target.addClass('active');
+           //remove all other active class from siblings
+           target.parent().siblings().children().eq(1).removeClass('active');
+        }else
+            target.removeClass('active');
+           
+        })
+       
+    }
+}
+
+index.init();
